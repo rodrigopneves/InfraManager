da3f4477a48d28bceadda1ef0e88d6a011f73a4c
Rodrigo Neves <rodrigo.pneves@gmail.com>
2026-09-11 10:59:15 -0300

    deploy: ajusta gunicorn para ambiente de producao

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 bind = "127.0.0.1:8000"
-workers = 2
+workers = 1
 worker_class = "sync"
 
 timeout = 30
@@ -10,3 +10,5 @@ accesslog = "-"
 errorlog = "-"
 capture_output = True
 access_log_format = '%(h)s "%(m)s %(U)s" %(s)s %(B)s %(L)s'
+
+control_socket_disable = True
